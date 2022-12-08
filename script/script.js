2d17dd0e95bc2053f079f4c4e3c169c42b832344
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2022-12-08 17:41:39 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,8 +3,7 @@ const elemLocation = document.querySelector(".location");
 const elemDate = document.querySelector(".date");
 const elemTemp = document.querySelector(".temp");
 const elemForm = document.querySelector(".form");
-
-const options = {
+const apiKey = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "f2902223d3mshf989ffd7ea8cccap190956jsn0dca5fd07b43",
@@ -15,44 +14,44 @@ const options = {
 elemForm.addEventListener("submit", (e) => {
   e.preventDefault();
   if (e.target[0].value) {
-    getData(e.target[0].value);
+    getWeatherData(e.target[0].value);
     elemForm.reset();
   }
 });
 
 
-function getStatus(response) {
-  return response.status === 200 ?
-    response.json() :
-    Promise.reject(new Error(response.status));
-}
-
-
-function getData(queryParameter) {
-  fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${queryParameter}`, options)
-    .then(getStatus)
-    .then(showWeatherData)
-    .catch(err => {
-      console.error(err);
-      showWeatherData();
-    });
-
-}
-
-
-function showWeatherData(data) {
+const showWeatherData = (data) => {
   elemWeathercon.innerHTML = data ? `<img src="${data.current.condition.icon}">` : "";
   elemLocation.innerText = data ? data.location.name : "лажа...  :(";
   elemDate.innerText = data ? data.location.localtime : "";
   elemTemp.innerText = data ? `${data.current.feelslike_c} °C` : "";
+};
+
+
+const catchError = (error) => {
+  showWeatherData();
+  console.error(error);
+};
+
+
+const sendHTTPRequest = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    if (response.status === 200) return await response.json();
+  } catch (error) {
+    catchError(error);
+  }
+};
+
+
+function getWeatherData(queryParameter) {
+  sendHTTPRequest(`https://weatherapi-com.p.rapidapi.com/current.json?q=${queryParameter}`, apiKey)
+    .then(showWeatherData)
+    .catch(catchError);
 }
 
 
 // start
-fetch("https://ipapi.co/json/")
-  .then(getStatus)
-  .then((r) => getData(r.ip))
-  .catch((err) => {
-    console.error(err);
-    showWeatherData();
-  });
+sendHTTPRequest("https://ipapi.co/json/")
+  .then((r) => getWeatherData(r.ip))
+  .catch(catchError);
