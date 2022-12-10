c5f86203d418f0490b2b1aa82f9074b6ac6929e2
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2022-12-10 21:43:56 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+const elemSpinner = document.querySelector(".spinner-wrap");
 const elemWeathercon = document.querySelector(".weathercon");
 const elemLocation = document.querySelector(".location");
 const elemDate = document.querySelector(".date");
@@ -25,10 +26,12 @@ const showWeatherData = (data) => {
   elemLocation.innerText = data ? data.location.name : "лажа...  :(";
   elemDate.innerText = data ? data.location.localtime : "";
   elemTemp.innerText = data ? `${data.current.feelslike_c} °C` : "";
+  elemSpinner.classList.remove("js-show-spinner");
 };
 
 
 const sendHTTPRequest = async (url, options) => {
+  elemSpinner.classList.add("js-show-spinner");
   try {
     const response = await fetch(url, options);
     if (response.status === 200) return await response.json();
